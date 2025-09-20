09a40fe7e7ab702d833c0d8b54a935106967c247
IAmBatJew <aamigarr@gmail.com>
2025-09-19 21:18:14 -0400

    Ugh

--- schakolad/src/App.js
+++ schakolad/src/App.js
@@ -4,7 +4,7 @@ import Home from './Home'
 
 function App() {
   return (
-    <Router>
+    <Router basename="/schakolad-chocolate-plainfield">
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
